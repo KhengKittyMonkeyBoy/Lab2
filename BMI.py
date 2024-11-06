36aada75eaa7635744844c0fdbe9844371046bed
Alfie <alfie.23@ichat.sp.edu.sg>
2024-11-06 10:57:45 +0800

    Modified bmi.py to return INTERGERRERERERER

--- BMI.py
+++ BMI.py
@@ -10,13 +10,20 @@ def calculate_bmi(height, weight):
 
 
     if BMI <18.5:
-        print("Underweight")    
+        print("Underweight")
+        return -1
+    
     elif BMI >25.0:
         print ("Overweight")
+        return 1
     else:
         print("Normal")
+        return 0
 
-calculate_bmi(weight="57", height="1.73")
+
+result = calculate_bmi(weight="57", height="1.73")
+result = calculate_bmi(weight="457", height="1.4")
+result = calculate_bmi(weight="57", height="1.90")
 
 
 
